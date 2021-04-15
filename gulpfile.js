6c9e12bd353dbea7932df169ef8d80aa2fe9cef0
Youmeng Hin <youmenghin@gmail.com>
2021-04-15 18:38:13 -0400

    Add htmlCompressor task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const { src, dest, series, watch } = require(`gulp`);
 const babel = require(`gulp-babel`);
 const htmlValidator = require(`gulp-html`);
+const htmlCompressor = require(`gulp-htmlmin`);
 
 let validateHTML = () => {
   return src([
@@ -9,4 +10,11 @@ let validateHTML = () => {
       .pipe(htmlValidator());
 };
 
+let compressHTML = () => {
+  return src([`dev/html/*.html`,`dev/html/**/*.html`])
+      .pipe(htmlCompressor({collapseWhitespace: true}))
+      .pipe(dest(`prod`));
+};
+
 exports.validateHTML = validateHTML;
+exports.compressHTML = compressHTML;
