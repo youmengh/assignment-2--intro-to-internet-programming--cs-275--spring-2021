0bf172faee9e30fc5b33de3ec0073f8f18a1468f
Youmeng Hin <youmenghin@gmail.com>
2021-04-15 18:43:21 -0400

    Add lintJS task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const { src, dest, series, watch } = require(`gulp`);
 const babel = require(`gulp-babel`);
 const htmlValidator = require(`gulp-html`);
 const htmlCompressor = require(`gulp-htmlmin`);
+const jsLinter = require(`gulp-eslint`);
 
 let validateHTML = () => {
   return src([
@@ -16,5 +17,30 @@ let compressHTML = () => {
       .pipe(dest(`prod`));
 };
 
+let lintJS = () => {
+  return src(`dev/scripts/*.js`)
+      .pipe(jsLinter({
+          parserOptions: {
+              ecmaVersion: 2017,
+              sourceType: `module`
+          },
+          rules: {
+              indent: [2, 4, {SwitchCase: 1}],
+              quotes: [2, `backtick`],
+              semi: [2, `always`],
+              'linebreak-style': [2, `unix`],
+              'max-len': [1, 85, 4]
+          },
+          env: {
+              es6: true,
+              node: true,
+              browser: true
+          },
+          extends: `eslint:recommended`
+      }))
+      .pipe(jsLinter.formatEach(`compact`, process.stderr));
+};
+
 exports.validateHTML = validateHTML;
 exports.compressHTML = compressHTML;
+exports.lintJS = lintJS;
