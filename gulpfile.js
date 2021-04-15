05c79bf25bef26d8866af8b0759685fee149ce40
Youmeng Hin <youmenghin@gmail.com>
2021-04-15 18:30:12 -0400

    Add htmlValidator task

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,12 @@
 const { src, dest, series, watch } = require(`gulp`);
 const babel = require(`gulp-babel`);
+const htmlValidator = require(`gulp-html`);
+
+let validateHTML = () => {
+  return src([
+      `dev/html/*.html`,
+      `dev/html/**/*.html`])
+      .pipe(htmlValidator());
+};
+
+exports.validateHTML = validateHTML;
